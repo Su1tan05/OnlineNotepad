71359254e7408fa937ffdbc757bd286a8d94943e
Султан <sultan-avto5006@mail.ru>
2021-05-13 08:02:40 +0300

    Added Unit tests

--- OnlineNotepad/Startup.cs
+++ OnlineNotepad/Startup.cs
@@ -22,19 +22,11 @@ namespace OnlineNotepad
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvc(routes =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
             });
         }
     }
